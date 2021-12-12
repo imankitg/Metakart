841e4f3798bdabbf34890c3e961b955485000e27
Ankit Kumar Gupta <ankitkumargupta262@gmail.com>
2021-12-12 20:03:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>Hello my first project that I am developing without using any tutorial</p>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
